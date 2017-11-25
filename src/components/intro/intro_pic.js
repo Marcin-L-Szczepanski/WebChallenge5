456e6b78682efd9997a01ff367c99cd46a4285ec
Marcin Szczepanski <marcin.l.szczepanski@gmail.com>
2017-11-25 17:57:02 +0900

    Change script for loading picture

--- src/components/intro/intro_pic.js
+++ src/components/intro/intro_pic.js
@@ -1,11 +1,19 @@
 "use strict";
 
 const logo = document.querySelector(".intro__pic");
-logo.setAttribute("src", "img/panther_min-min.jpg");
+logo.src = "img/panther_min-min.jpg";
 logo.classList.add("intro__pic--loading");
 
-logo.addEventListener("load", function(event) {
-  
-logo.setAttribute("src", "img/panther.jpg");
-logo.classList.remove("intro__pic--loading");
-});
+function loaded() {
+  logo.src = "img/panther.jpg";
+  logo.classList.remove("intro__pic--loading");
+}
+
+if (logo.complete) {
+  loaded()
+} else {
+  logo.addEventListener('load', loaded)
+  logo.addEventListener('error', function() {
+      alert('error')
+  })
+}
