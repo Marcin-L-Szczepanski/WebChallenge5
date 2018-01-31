926776ff5bbf83ae4fd4eb397764314da4adeaf2
Marcin Szczepanski <marcin.l.szczepanski@gmail.com>
2018-01-31 22:50:33 +0900

    Hide and show menu button on scrolling

--- src/components/buttons/button--menu.js
+++ src/components/buttons/button--menu.js
@@ -1,16 +1,26 @@
 const showHideButton = document.createElement("button");
 const menuList = document.querySelector("#menu__list");
+let currentPosition = 0;
 
 (function createMenuButton() {
   "use strict";
   const menu = document.querySelector("#menu");
   
   showHideButton.innerHTML = "Menu";
-  showHideButton.classList.add("menu__button", "button");
+  showHideButton.classList.add("menu__button", "menu__button--hidden", "button");
   showHideButton.setAttribute("type", "button");
   showHideButton.setAttribute("aria-controls", "menu__list")
   menu.insertBefore(showHideButton, menu.firstElementChild);
   }());
+
+function hideShowMenuButton () {
+  if (currentPosition > window.pageYOffset) { 
+    showHideButton.classList.remove("menu__button--hidden"); 
+  } else {
+    showHideButton.classList.add("menu__button--hidden");
+  }
+  currentPosition = window.pageYOffset;
+}
 
 function hideMenu () {
   showHideButton.setAttribute("aria-expanded", "false");
@@ -21,6 +31,10 @@ function showMenu () {
   showHideButton.setAttribute("aria-expanded", "true");
   menuList.classList.remove("menu__list--hidden");
 }
+
+document.addEventListener("scroll", function() {
+    hideShowMenuButton();
+});
 
 showHideButton.addEventListener("click", function() {
   if (menuList.classList.contains("menu__list--hidden")) {
