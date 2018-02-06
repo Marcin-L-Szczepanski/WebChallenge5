ad9d32e6cbaaf83e63f68d994e7792a3e5a7c4f4
Marcin Szczepanski <marcin.l.szczepanski@gmail.com>
2018-02-06 22:30:31 +0900

    Change close button

--- src/components/postcard/postcard.js
+++ src/components/postcard/postcard.js
@@ -11,7 +11,7 @@ for (var i = 0; i < postcard.length; i++) {
     postcardModal.classList.add("postcard__modal");
     postcardModalPicture.classList.add("postcard__modal-picture");
     postcardModalButton.classList.add("postcard__modal-button");
-    postcardModalButton.innerHTML = "X";
+    postcardModalButton.innerHTML = '<svg width="50" height="50" xmlns="http://www.w3.org/2000/svg" version="1.1"><g stroke="#dfdfdf"  stroke-width="10" ><line x1="10" x2="40" y1="10" y2="40" /> <line x1="10" x2="40" y1="40" y2="10" /></g></svg>';
     galleryBody.appendChild(postcardModal);
     postcardModal.appendChild(postcardModalPicture);
     postcardModal.appendChild(postcardModalButton);
