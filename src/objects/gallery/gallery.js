b4e1e36dcf0375a2749b3d40c53b1d3dc0e3d974
Marcin Szczepanski <marcin.l.szczepanski@gmail.com>
2018-02-14 00:09:56 +0900

    Use ES6 arrow function

--- src/objects/gallery/gallery.js
+++ src/objects/gallery/gallery.js
@@ -1,19 +1,15 @@
-let link = document.querySelectorAll(".gallery__category");
-link.forEach(function (element) {
-  element.addEventListener("click", function() {
-    for(let i = 0; i < link.length; i++) {
-      link[i].classList.add("gallery__category--inactive");
-    }
-    element.classList.remove("gallery__category--inactive");
+let categories = document.querySelectorAll(".gallery__category");
+categories.forEach(function (category) {
+  category.addEventListener("click", function () {
+    categories.forEach(link => link.classList.add("gallery__category--inactive"));
+    this.classList.remove("gallery__category--inactive");
 
     let postcards = document.querySelectorAll(".postcard");
-    if(element.id == "categoryPrint") {
+    if (category.id == "categoryPrint") {
       postcards.forEach(postcard => postcard.classList.contains("web") ? postcard.style.display = "none" : postcard.style.display = "block");
-    }
-    else if(element.id == "categoryWeb") {
+    } else if (category.id == "categoryWeb") {
       postcards.forEach(postcard => postcard.classList.contains("print") ? postcard.style.display = "none" : postcard.style.display = "block");
-    }
-    else {
+    } else {
       postcards.forEach(postcard => postcard.style.display = "block");
     }
   }, false);
