49a482dad85ef2cedd9156c0557f30fa24662130
Marcin Szczepanski <marcin.l.szczepanski@gmail.com>
2018-02-15 21:29:08 +0900

    Style slider buttons on hover

--- src/tools/opinions-slider.js
+++ src/tools/opinions-slider.js
@@ -15,8 +15,8 @@ function createSliderControlers() {
 function createButtons() {
   const btnLeft = document.createElement('button');
   const btnRight = document.createElement('button');
-  btnLeft.innerHTML = '<svg width="30px" height="30px" viewBox="0 0 60 80" xml:space="preserve"><polyline fill="none" stroke="#000000" stroke-width="25" stroke-linecap="round" stroke-linejoin="round" points="45,65 5,38 45,15 "/></svg>';
-  btnRight.innerHTML = '<svg width="30px" height="30px" viewBox="0 0 40 80" xml:space="preserve"><polyline fill="none" stroke="#000000" stroke-width="25" stroke-linecap="round" stroke-linejoin="round" points="0,15 45,38 0,65 "/></svg>';
+  btnLeft.innerHTML = '<svg width="30px" height="30px" viewBox="0 0 60 80" xml:space="preserve"><polyline class="opinion__button-arrow" points="45,65 5,38 45,15 "/></svg>';
+  btnRight.innerHTML = '<svg width="30px" height="30px" viewBox="0 0 40 80" xml:space="preserve"><polyline class="opinion__button-arrow" points="0,15 45,38 0,65 "/></svg>';
   btnLeft.classList.add("opinion__button", "opinion__button--left");
   btnRight.classList.add("opinion__button", "opinion__button--right");
   const box = document.querySelector(".siema");
