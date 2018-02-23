de02c103f6714027927f031b19b1c1d67ddbffdb
Marcin Szczepanski <marcin.l.szczepanski@gmail.com>
2018-02-23 23:22:47 +0900

    Create previous and next buttons

--- src/components/postcard/postcard.js
+++ src/components/postcard/postcard.js
@@ -15,15 +15,26 @@ document.addEventListener("click", function(e) {
 
 function openModal(e) {
   const postcardModal = document.createElement("div");
+  const postcardModalButtonParrent = document.createElement("div");
   const postcardModalPicture = document.createElement("img");
   const postcardModalCloseButton = document.createElement("button");
+  const postcardModalPreviousButton = document.createElement("button");
+  const postcardModalNextButton = document.createElement("button");
   postcardModal.classList.add("postcard__modal");
+  postcardModalButtonParrent.classList.add("postcard__button-parrent");
   postcardModalPicture.classList.add("postcard__modal-picture");
   postcardModalCloseButton.classList.add("postcard__modal-button");
-  postcardModalCloseButton.innerHTML = '<svg width="50" height="50" xmlns="http://www.w3.org/2000/svg" version="1.1"><g stroke="#dfdfdf"  stroke-width="10" ><line x1="10" x2="40" y1="10" y2="40" /> <line x1="10" x2="40" y1="40" y2="10" /></g></svg>';
+  postcardModalPreviousButton.classList.add("postcard__modal-button");
+  postcardModalNextButton.classList.add("postcard__modal-button");
+  postcardModalPreviousButton.innerHTML = '<svg width="50" height="50" xmlns="http://www.w3.org/2000/svg" version="1.1"><g stroke="#dfdfdf"  stroke-width="10" ><line x1="40" y1="10" x2="26" y2="25" /> <line x1="22" y1="22" x2="40" y2="40" /></g></svg>';
+  postcardModalCloseButton.innerHTML = '<svg width="50" height="50" xmlns="http://www.w3.org/2000/svg" version="1.1"><g stroke="#dfdfdf" stroke-width="10"><line x1="10" y1="10" x2="40" y2="40" /> <line x1="10" x2="40" y1="40" y2="10" /></g></svg>';
+  postcardModalNextButton.innerHTML = '<svg width="50" height="50" xmlns="http://www.w3.org/2000/svg" version="1.1"><g stroke="#dfdfdf" stroke-width="10" ><line x1="10" y1="10" x2="28" y2="30" /><line x1="10" y1="40" x2="25" y2="25" /> </g></svg>';
   galleryBody.appendChild(postcardModal);
   postcardModal.appendChild(postcardModalPicture);
-  postcardModal.appendChild(postcardModalCloseButton);
+  postcardModal.appendChild(postcardModalButtonParrent);
+  postcardModalButtonParrent.appendChild(postcardModalPreviousButton);
+  postcardModalButtonParrent.appendChild(postcardModalCloseButton);
+  postcardModalButtonParrent.appendChild(postcardModalNextButton);
   postcardModalPicture.src = e.target.parentElement.parentElement.firstElementChild.src;
   let currentPostcard = e.target.parentElement.parentElement;
   
